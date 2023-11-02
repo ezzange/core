8548ca8b268c3dc1bdf4e27618b1b8e99b93606a
ezzange <ezzang22@naver.com>
2023-11-02 17:08:13 +0900

    spring findBean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,32 +1,31 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberverServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//구현 객체를 생성하고 인스턴스 객체를 생성자 주입으로 연결해준다.
-//구현체가 변경될 경우 여가의 코드만 변경 해주면 된다.
+@Configuration
 public class AppConfig {
-    //메서드명과 리턴타입만 봐도 역할과 구현 클래스가 한 눈에 들어와 애플리케이션의 전체 구성을 한 번에 파악할 수 있다.
+    @Bean
     public MemberService memberService() { // memberService의 구현은 memberServiceImpl가 할 것이다.
         return new MemberverServiceImpl(memberRepository());
     }
-
+    @Bean
     public MemoryMemberRepository memberRepository() { // memberRepository의 구현은 MemoryMemberRepository가 할 것이다.
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() { // orderService의 구현은 OrderServiceImpl.
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy(); //discountPolicy의 구현은 FixDiscountPolicy.
-        return new RateDiscountPolicy(); // discountPolicy의 구현은 RateDiscountPolicy.
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-//        MemberService memberService = new MemberverServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+                                                //AppConfig의 있는 내용으로 객체를 생성 및 관리를 해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+                                                                    // 꺼내올 객체의 이름과 타입
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
