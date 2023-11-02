b4d3c72c7de7407d5fd0c73248b042f9be72eafc
ezzange <ezzang22@naver.com>
2023-11-02 20:18:46 +0900

    singleton

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -19,16 +19,20 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA : A사용자가 10000원 주문
-        statefulService1.order("userA",10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadB : B사용자가 10000원 주문
-        statefulService2.order("userB",20000);
+        int userBPrice = statefulService2.order("userB", 20000);
+
 
         //ThreadA : A사용자 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+//        System.out.println("price = " + price);
         //A사용자의 주문 금액을 조회 하는데 B사용자의 주문 금액이 나온 이유는 인스턴스 변수 값을 공유하기 때문이다.
         //가장 마지막으로 할당된 값이 전체적으로 공유 되어 버린다.
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+        System.out.println("userAPrice = " + userAPrice);
+        System.out.println("userBPrice = " + userBPrice);
     }
 
     static class TestConfig {
